ea7ec59d788b2fa27479c8664c0bda39edeaba0f
Sandesh1203 <92106697+Sandesh1203@users.noreply.github.com>
2026-02-11 01:48:10 +0530

    Create user-data-server-1a.sh

---
+++ scripts/user-data-server-1a.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+yum update -y
+yum install -y nginx
+
+rm -f /usr/share/nginx/html/index.html
+
+cat > /usr/share/nginx/html/index.html << EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body style="text-align: center; font-family: Arial; margin-top: 100px;">
+    <h1>Hello World!</h1>
+    <p>Instance ID: <strong> i-0425f5525cc4ced57 </strong></p>
+    <p>Availability Zone: <strong>us-east-1a</strong></p>
+    <p>Served by: <strong>Nginx</strong></p>
+</body>
+</html>
+EOF
+
+chmod 644 /usr/share/nginx/html/index.html
+systemctl start nginx
+systemctl enable nginx
